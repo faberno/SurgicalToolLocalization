d136109ea8b975cef8bf676a8cf66a552599f623
Fabian Schneider <fabianschneider2609@gmail.com>
2022-09-13 00:56:45 +0200

    added script to easily zip important files for colab

---
+++ makezip.py
@@ -0,0 +1,21 @@
+import os
+import zipfile
+
+with zipfile.ZipFile("files.zip", "w") as zf:
+    for dirname, subdirs, files in os.walk("./"):
+        if dirname == './':
+            for filename in files:
+                if filename in ['files.zip', '.gitignore', 'README.md', 'LICENSE']:
+                    continue
+                zf.write(filename)
+            continue
+        continuehere = False
+        for notallowed in ['./checkpoints', './.git', './.idea', './report']:
+            if dirname.startswith(notallowed):
+                continuehere = True
+                break
+        if continuehere:
+            continue
+        zf.write(dirname)
+        for filename in files:
+            zf.write(os.path.join(dirname, filename))
